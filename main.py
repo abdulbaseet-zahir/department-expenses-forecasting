0de61cc520f7d3b41d3cf7db09e1c6999a50c554
Abdulbasit Zahir <61330992+abdulbaseet-zahir@users.noreply.github.com>
2024-06-03 15:02:33 +0300

    add unit forcasting

--- main.py
+++ main.py
@@ -1,67 +1,73 @@
 from fastapi import FastAPI
 import uvicorn
-
 import pandas as pd
 from prophet import Prophet
 
-data_path = "data/model.csv"
-
-
-def get_data(data_path):
-    data_df = pd.read_csv(data_path)
-    data_df["Months"] = pd.to_datetime(data_df["Months"])
-    data_df = (
-        data_df.groupby(["Department_ID", "Months"])["Expenses"].sum().reset_index()
-    )
-    data_df = data_df.pivot(
-        index="Months", columns="Department_ID", values="Expenses"
-    ).reset_index()
-
-    return data_df
-
-
-def get_one_year_forcast(ID):
-    data_df = get_data(data_path)
-    data = data_df[["Months", ID]].rename(columns={"Months": "ds", ID: "y"})
-
-    model = Prophet()
-    model.fit(data)
-    future = model.make_future_dataframe(periods=12, freq="M")
-    forecast = model.predict(future)
-    return forecast[["ds", "yhat"]]
-
-
-def get_all_expenses(ID):
-    data_df = get_data(data_path)
-    data = data_df[["Months", ID]].rename(columns={"Months": "ds", ID: "y"})
-
-    model = Prophet()
-    model.fit(data)
-    future = model.make_future_dataframe(periods=12, freq="M")
-    forecast = model.predict(future)
-
-    future["y"] = data["y"]
-    future["yhat"] = forecast["yhat"]
-    future.fillna(0, inplace=True)
-    return future
-
+data_path = "data/data.csv"
 
 app = FastAPI()
 
 
-@app.get("/get_one_year_forcast/{ID}")
-async def square(ID: int):
-    return get_one_year_forcast(ID).to_dict(orient="records")
+def get_data(data_path: str) -> pd.DataFrame:
+    data_df = pd.read_csv(data_path)
+    data_df["Months"] = pd.to_datetime(data_df["Months"])
+    data_df["Unit_ID"] = data_df["Unit_ID"].map(lambda x: int(str(x)[2:]))
+    return data_df
 
 
-@app.get("/get_all_expenses/{ID}")
-async def square(ID: int):
-    return get_all_expenses(ID).to_dict(orient="records")
+def prepare_data(
+    data_df: pd.DataFrame, department_id: int, unit_id: int = None
+) -> pd.DataFrame:
+    if unit_id:
+        grouped_df = (
+            data_df.groupby(["Department_ID", "Unit_ID", "Months"])["Expenses"]
+            .sum()
+            .reset_index()
+        )
+        pivoted_df = grouped_df.pivot(
+            index="Months", columns=["Department_ID", "Unit_ID"], values="Expenses"
+        ).reset_index()
+        months = pivoted_df["Months"]
+        department_data = pd.concat([months, pivoted_df[department_id]], axis=1)
+        return department_data[["Months", unit_id]].rename(
+            columns={"Months": "ds", unit_id: "y"}
+        )
+    else:
+        grouped_df = (
+            data_df.groupby(["Department_ID", "Months"])["Expenses"].sum().reset_index()
+        )
+        pivot_df = grouped_df.pivot(
+            index="Months", columns="Department_ID", values="Expenses"
+        ).reset_index()
+        return pivot_df[["Months", department_id]].rename(
+            columns={"Months": "ds", department_id: "y"}
+        )
+
+
+def create_and_predict_model(data: pd.DataFrame, periods: int = 12) -> pd.DataFrame:
+    model = Prophet()
+    model.fit(data)
+    future = model.make_future_dataframe(periods=periods, freq="M")
+    forecast = model.predict(future)
+    return forecast[["ds", "yhat"]]
+
+
+# optional unit id
+@app.get("/get_department_expense_forcasts/{department_id}")
+async def get_department_expense_forcasts(
+    department_id: int, unit_id: int = None
+) -> list[dict]:
+    data_df = get_data(data_path)
+    data = prepare_data(data_df, department_id, unit_id)
+    forecast = create_and_predict_model(data)
+
+    future_data = forecast.merge(data, on="ds", how="left")
+    # future_data.fillna(0, inplace=True)
+    future_data.rename(
+        columns={"ds": "Months", "y": "Actuals", "yhat": "Forecast"}, inplace=True
+    )
+    return future_data.to_dict(orient="records")
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        app,
-        host="127.0.0.1",
-        port=8080,
-    )
+    uvicorn.run(app, host="127.0.0.1", port=8080)
